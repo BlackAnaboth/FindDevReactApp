b94f530f2365400164515e22d826858017a391d8
BlackAnaboth <BLACKANABOTH@yandex.com>
2023-03-13 13:12:35 -0500

    Repo inicial paratrabajar Hooks

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useReducer } from "react";
+import DetailCard from "./components/DetailCard";
+import EmptyCard from "./components/EmptyCard";
+import History from "./components/History";
+import "./App.css";
+
+const initialState = {
+  search: "",
+  searchHistory: [],
+};
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Devfinder</h1>
+        <form>
+          <label htmlFor="search">
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search Github username_"
+              autoComplete="off"
+            />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+      </header>
+      <div className="result">
+        <DetailCard />
+      </div>
+      <History />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DetailCard.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from "react-query";
+import EmptyCard from "./EmptyCard";
+
+export default function DetaildCard({ user, handleHistory }) {
+  return (
+    <div className="detail">
+      <div className="left">
+        <img 
+          className="avatar" 
+          src="https://avatars.githubusercontent.com/u/583231?v=4" 
+          alt="" 
+        />
+      </div>
+      <div className="right">
+        <div className="title">
+          <h2>Octocat</h2>
+          <a href="#">@octocat</a>
+        </div>
+        <div className="stats">
+          <div className="item">
+            <small>Repos</small>
+            <span>1</span>
+          </div>
+          <div className="item">
+            <small>Followers</small>
+            <span>15</span>
+          </div>
+          <div className="item">
+            <small>Following</small>
+            <span>24</span>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <i className="fas fa-map-marker-alt"> "Not available"</i>
+          </li>
+          <li>
+            <i className="fab fa-twitter"> "Not available"</i>{" "}
+          </li>
+          <li>
+            <i className="fas fa-link">"Not available"</i>{" "}
+          </li>
+          <li>
+            <i className="fas fa-building"> "Not available" </i>{" "}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
